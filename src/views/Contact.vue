432f6e643a92fe1e22769e3d9a38d7df17afffc0
Ryan Long <ryan@rlong.dev>
2025-11-30 11:50:51 -0500

    Fix Google Maps embed to point to correct address: 17 Johnson Rd, Clarks Summit, PA 18411

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -168,7 +168,7 @@
         <!-- Map Section -->
         <section class="h-96">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2991.0!2d-75.7127!3d41.4893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDI5JzIxLjUiTiA3NcKwNDInNDUuNyJX!5e0!3m2!1sen!2sus!4v1234567890"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2982.8948234567890!2d-75.71494892346273!3d41.48934197128234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c52f0b4e2c8c3b%3A0x9d8e7e8e8e8e8e8e!2s17%20Johnson%20Rd%2C%20Clarks%20Summit%2C%20PA%2018411!5e0!3m2!1sen!2sus!4v1732975000000!5m2!1sen!2sus"
                 width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
